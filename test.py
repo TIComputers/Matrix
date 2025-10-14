ea71bb4ab650eb6a989ddc260fdabc6f96e3c78a
TIComputer <Computer_CoDism@proton.me>
2025-10-14 20:14:32 +0330

    Update
    Modifying matrix multiplication operation codes

--- test.py
+++ test.py
@@ -1,86 +1,6 @@
-from time import time, sleep
-from test2 import *
-
-def ATM(Matrix:list, row:int, col:int) -> int:
-    I = Matrix[row]
-    a = I[col]
-    return a
-
-
-def NRC(Matrix:list) -> str:
-    """ Number of rows and columns,
-        Order of the matrix """
-    # number of row in matris
-    row = 0
-    for i in Matrix:
-        row += 1
-
-    # number of column in matris
-    col = 0
-    for j in Matrix[1]:
-        col += 1
-
-    return (row, col)
-
-
-def MDS (Matrix:list) -> bool:
-    """ Matrix defines structure
-        Columns per row """
-    RL = []   # length row
-    
-    for row in Matrix:   # Getting the number of items in each row
-        L = len(row)
-        RL.append(L) 
-          
-    for time in RL: # Checking for equality of items
-        if time != L:
-            exit("Your list is not a Matrix")
-
-  
-    
-def NRC(Matrix:list) -> str:
-    """ Number of rows and columns,
-        Order of the matrix """
-    # number of row in matris
-    row = 0
-    for i in Matrix:
-        row += 1
-
-    # number of column in matris
-    col = 0
-    for j in Matrix[1]:
-        col += 1
-        
-    return (row, col)
-
-
-def MA(Matrix1:list, Matrix2:list) -> list:
-    """ Matrix Addition """
-    RC1 = NRC(Matrix1)
-    RC2 = NRC(Matrix2)
-
-    if RC1 != RC2:  # red line
-        exit(f"Cann`t Adding Matrix1{RC1} with Matrix2{RC2}")
-    
-    NM = [] # new matrix for addition matrix1 to matrix2
-    
-    for i in range(RC1[0]):
-        NR = [] # new row for new matrix
-        for j in range(RC1[1]):
-            sum = ATM(Matrix1, i, j) + ATM(Matrix2, i, j)
-            NR.append(sum)
-        NM.append(NR)
-    return NM
-
-
-def TC(Matrix:list, col:int) -> list:
-    """ The columns """
-
-    columns = [] # new list for write columns list
-    for row in Matrix:
-        columns.append(row[col])
-    return columns
-
+from Main import *
+from Examples import *
+from time import sleep, time
 
 def MMS(Matrix1:list, Matrix2:list) -> list:
     """ Multiply matrices systematically """
@@ -91,16 +11,22 @@ def MMS(Matrix1:list, Matrix2:list) -> list:
             exit(f"Cann`t Multiply matrix -> {RC1} * {RC2}")
 
     NM = [] # new matrix for multiply matrix1 to matrix2
- 
-    for row in Matrix1:
-        NR = [] # new row for add new matrix multiplyed
+
+    for row in Matrix1: # row is a list from Matrix1
+        NR = [] # new row form New Matrix
         for col in range(RC2[1]):
-            COL = TC(Matrix2, col)
+            Col = TC(Matrix2, col)
             sum = 0
-            for i in range(RC2[1]):
+            for i in range(RC1[1]):
                 mul = 0
-                mul = row[i] * COL[i]
+                mul = row[i] * Col[i]
                 sum += mul
             NR.append(sum)
         NM.append(NR)
     return NM
+        
+        
+start = time()
+print(MMS(matrix_10x10_a, matrix_10x10_b))
+stop  = time()
+print(f"Time: {stop - start}")
--- test3.py
+++
@@ -1,8 +0,0 @@
-
-def TC(Matrix:list, col:int, ) -> list:
-    """ The columns """
-
-    columns = [] # new list for write columns list
-    for row in Matrix:
-        columns.append(row[col])
-    return columns
