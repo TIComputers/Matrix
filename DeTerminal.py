2c0f50a4510fd1f0213b4827f8b640bae0d730c1
TIComputer <Computer_CoDism@proton.me>
2025-10-17 17:04:45 +0330

    Updata
    Update: Corrected the code and added the code to check the lower triangular matrix.

--- DeTerminal.py
+++ DeTerminal.py
@@ -1,5 +1,32 @@
 import Main
 
+def TB(Matrix:list) -> bool:
+    """ Triangular bottom """
+    
+    check_TB: list = []
+    LR: int = len(Matrix[0]) # lenght row in matrix
+    
+    for indx_r in range(LR -1):
+        indx_cc = indx_r + 1
+        check_r: list = []
+        
+        for indx_c in range(indx_cc, LR):
+            num = Main.ATM(Matrix, indx_r, indx_c)
+            
+            if num == 0:
+                check_r.append(True)
+            else:
+                return False
+                break
+        
+        if all(check_r):
+            check_TB.append(True)
+        
+    if all(check_TB):
+        return True
+    else:
+        return False
+
 
 def TT(Matrix:list) -> bool:
     """ Triangular top """
@@ -15,95 +42,17 @@ def TT(Matrix:list) -> bool:
             if num == 0:
                 check_r.append(True)
             else:
-                check_r.append(False)
+                return False
+                break 
             
         if all(check_r):
             check_TT.append(True)
-        else: 
-            check_TT.append(False)
  
     if all(check_TT):
         return True
     else:
         return False
         
-
-        
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-def TB():
-    """ Triangular bottom """
-    pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 def DTT():
     """ Deterministic two by two """
